bc9642d7e862ac21e2d57a69b2640f9971a7a872
minju-kim98 <ducco705@snu.ac.kr>
2022-07-07 10:25:10 +0900

    commit

---
+++ PJ3/slow_version/go.mod
@@ -0,0 +1,10 @@
+module github.com/minju-kim98/learngo/PJ3/slow_version
+
+go 1.18
+
+require github.com/PuerkitoBio/goquery v1.8.0
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ PJ3/slow_version/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+func main() {
+	totalPages := getPageNum()
+
+	for i := 0; i < totalPages; i++ {
+		getPageInfo(i)
+	}
+
+}
+
+func getPageNum() int {
+	pages := 0
+
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length() //find all the links
+	})
+
+	return pages
+}
+
+func getPageInfo(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".job_seen_beacon")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		extractJob(card)
+	})
+
+}
+
+func extractJob(card *goquery.Selection) {
+	//id, _ := card.Attr("data-jk")
+	//title := cleanString(card.Find(".title").Text())
+	//location := cleanString(card.Find(".companyLocation").Text())
+	//fmt.Println(id, title, location)
+
+	id, exist := card.Attr("data-jk")
+	fmt.Println(id, exist)
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("status code error: ", res.StatusCode, res.Status)
+	}
+}
